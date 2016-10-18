311013896be68043930ae503d1739845c9882537
Shinsuke Hamasho <sh.hamasho@gmail.com>
2016-10-19 08:31:42 +0900

    Change source and dest directory of SCSS compiler

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,11 +2,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('styles', function() {
-  gulp.src('core/static/css/*.scss')
+  gulp.src('apps/core/static/css/*.scss')
       .pipe(sass().on('error', sass.logError))
-      .pipe(gulp.dest('core/static/css/'));
+      .pipe(gulp.dest('apps/core/static/css/'));
 });
 
 gulp.task('default', function() {
-  gulp.watch('core/static/css/*.scss', ['styles']);
+  gulp.watch('apps/core/static/css/*.scss', ['styles']);
 });
